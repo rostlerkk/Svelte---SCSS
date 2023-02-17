6708f35ef078a1a7945e52ec73b853ff409d8072
Zone360.sk <75951610+rostlerkk@users.noreply.github.com>
2023-02-17 13:08:44 +0100

    update

---
+++ src/components/Menu.svelte
@@ -0,0 +1,36 @@
+<script>
+
+    async function fetchData($url, $lang, $type, $variable) {
+        
+        $variable = false;
+        if (_vivaData[$lang] == undefined) {
+            const res = await fetch($url);
+            const json = await res.json();
+
+            if (res.ok) {
+
+                if (_vivaData[$lang] == undefined) {
+                    _vivaData[$lang] = {
+                        houses : null,
+                        subtitles : null,
+                        products : null
+                    };
+                }
+                _vivaData[$lang][$type] = json; 
+                vivaData.update(n => _vivaData);       
+
+                if ($type == "houses") {
+                    console.log(_vivaData);
+                }
+
+            } else {
+                throw new Error(json);
+            }            
+        }    
+        $variable = true;
+        
+    }
+
+    const url = "https://int.baumit.com/api/buildings?api_token=AP7R6yAeBytyvc0OASz3RhLrAH2CsCGnNDWTeLBAbyW72sMPRCiw4JAKDTz0";
+    console.log(url);
+</script>
