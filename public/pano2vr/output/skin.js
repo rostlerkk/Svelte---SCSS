52734d0650636266d3491939fc480f34325c9cd7
Zone360.sk <75951610+rostlerkk@users.noreply.github.com>
2023-02-22 09:19:39 +0100

    update

--- public/pano2vr/output/skin.js
+++ public/pano2vr/output/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.15/18116
 // Filename: baumit-viva-park.ggsk
-// Generated 2023-02-22T08:41:38
+// Generated 2023-02-22T09:11:27
 
 function pano2vrSkin(player,base) {
 	player.addVariable('lang', 0, "");
