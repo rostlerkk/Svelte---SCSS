294c6201216aa3ccae78faab30f8e6550925459d
Zone360.sk <75951610+rostlerkk@users.noreply.github.com>
2023-04-21 09:02:47 +0200

    update linku pre PHP

--- src/components/Vivapark.svelte
+++ src/components/Vivapark.svelte
@@ -1315,9 +1315,9 @@ async function fetchPhpData($lang) {
     }
 
     //console.log(api_data);
-    let phpUrl = "https://woowstudio.com/vyvoj/getValues.php";
+    //let phpUrl = "https://woowstudio.com/vyvoj/getValues.php";
     //let phpUrl = "https://tour.baumit.com/assets/krpano/getValues.php";
-    //let phpUrl = "assets/php/getValues.php";
+    let phpUrl = "assets/php/getValues.php";
 
     const res = await fetch(phpUrl, {
         method: 'POST',
